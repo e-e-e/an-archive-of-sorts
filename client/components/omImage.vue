ed4c27624db8ea615ade4ba7043e2a5936bd0721
e-e-e <e-e-e@users.noreply.github.com>
2017-02-07 22:20:06 +1100

    start omImage component for galleries.

---
+++ client/components/omImage.vue
@@ -0,0 +1,28 @@
+<template>
+  <img :src="imageSrc"></img>
+</template>
+
+<script>
+
+export default {
+  name: 'omImage',
+  props: {
+    file: {
+      type: Object,
+    },
+  },
+  data() {
+  },
+  computed: {
+    imageSrc() {
+      const { original_path: dir, original_name: name } = this.file;
+      return `${dir}/${name}`;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
